682f16970c678a1567ddadf477cb1930511ac70f
cwijayasundara <cwijayasundara@gmail.com>
2018-01-18 16:42:46 +0000

    Trying to fix the int test

--- src/test/java/com/cham/samplebootbatchapp/SampleBootBatchAppApplicationTests.java
+++ src/test/java/com/cham/samplebootbatchapp/SampleBootBatchAppApplicationTests.java
@@ -1,16 +1,36 @@
 package com.cham.samplebootbatchapp;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes=DemoJobDef.class)
 public class SampleBootBatchAppApplicationTests {
+
+	// ref: https://docs.spring.io/spring-batch/4.0.x/reference/html/testing.html#testing
+	private JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
+
+	@Before
+	public void setUp(){
+
+	}
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void hasTheJobCompleted() throws Exception {
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+		Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
 	}
 
 }
